e49146d04524bdd21b2e61e31ebbb4e60317ee34
alexhire <alex.hire@gmail.com>
2014-09-16 23:20:15 +0300

    Initial commit

---
+++ src/com/mhm/Main.java
@@ -0,0 +1,18 @@
+package com.mhm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("1");
+        // 3
+        // 4
+        // 5
+        // 6
+        // 7
+        // 8
+        // 9
+        // b1
+        // b2
+    }
+}
