315e9c59592d70630f0048bc3e9f7fb150024ce5
alexhire <alex.hire@gmail.com>
2014-09-17 11:27:13 +0300

    1

--- src/com/mhm/Main.java
+++ src/com/mhm/Main.java
@@ -5,21 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("1");
-        // 3
-        // 4
-        // 5
-        // 6
-        // 7
-        // 8
-        // 9
-        // b1
-        // b2
 
-        // 1
-        // 2
-        // m1
-        // m2
-        // 3
-        // 4
     }
 }
